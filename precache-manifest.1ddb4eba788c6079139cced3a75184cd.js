cd6777d0b4e0daffae7acb3d5184e81b61d2b098
Sowce <lyoxchan@gmail.com>
2019-12-28 00:13:46 +0100

    ba34db7

---
+++ precache-manifest.1ddb4eba788c6079139cced3a75184cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cf732ed41e48da867766f3826d41d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f683b063cc6f2287ee5",
+    "url": "/static/css/main.d94c1df0.chunk.css"
+  },
+  {
+    "revision": "697e60ad42f8a0caf536",
+    "url": "/static/js/2.2510f125.chunk.js"
+  },
+  {
+    "revision": "f2d92af21f4280d9b0181f77c3841133",
+    "url": "/static/js/2.2510f125.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f683b063cc6f2287ee5",
+    "url": "/static/js/main.98ec3b5b.chunk.js"
+  },
+  {
+    "revision": "a3410cd3f852b8bd790c",
+    "url": "/static/js/runtime-main.88f6b016.js"
+  }
+]);
